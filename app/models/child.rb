13f5084eb803d27890807442a808848baaa7670c
Daniel Li <tcli@andrew.cmu.edu>
2021-02-08 08:15:55 -0500

    points_earned  added to child model

--- app/models/child.rb
+++ app/models/child.rb
@@ -11,5 +11,9 @@ class Child < ApplicationRecord
   def name
     first_name + " " + last_name
   end
+
+  def points_earned
+    self.chores.done.inject(0){|sum,chore| sum += chore.task.points}
+  end 
     
 end
