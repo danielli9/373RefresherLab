9a8525ce7fc66a9154edec62485976c561be9394
Daniel Li <tcli@andrew.cmu.edu>
2021-02-08 08:05:26 -0500

    completed task model, 100% tests pass

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,11 @@
 class Task < ApplicationRecord
+  has_many :chores
+  has_many :children, through: :chores
+
+  validates_presence_of :name, :points
+  validates :points, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  scope :alphabetical, -> { order(:name) }
+  scope :active, -> { where('active = ?', true) }
+
 end
